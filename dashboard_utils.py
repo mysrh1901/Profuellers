60e0e48b19b4232e14ad38735884505dd1cf6b7b
Research <Research@macbookpro.mynetworksettings.com>
2026-08-10 19:16:14 -0400

    fix: Set baseline debt to /bin/zsh when no findings (was K)

--- dashboard_utils.py
+++ dashboard_utils.py
@@ -74,7 +74,7 @@ class DashboardData:
         self.twins[0].overall_score = 87.0
         self.twins[0].trend = "STABLE"
         self.twins[0].open_risks = 0
-        self.twins[0].compliance_debt_usd = 50000
+        self.twins[0].compliance_debt_usd = 0
         self.twins[0].audit_readiness_pct = 92.0
         self.twins[0].security_findings = []
         self.twins[0].domain_scores = {
